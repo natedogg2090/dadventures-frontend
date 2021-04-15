fa22eb0467d40c10a42dccbb27f33af7bb14adad
Nate Dietz <nsdietz@gmail.com>
2021-04-15 20:15:39 +0100

    added state management

--- src/components/activities/ActivityInput.js
+++ src/components/activities/ActivityInput.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 class ActivityInput extends Component {
     state = {
-        activityName: '',
-        activityDescription: '',
-        activityDuration: '',
-        activityWhatToBring: '',
-        activityWhatToDo: ''
+        activityName: " ",
+        activityDescription: " ",
+        activityDuration: " ",
+        activityWhatToBring: " ",
+        activityWhatToDo: " "
+    }
+
+    handleOnChange = (e) => {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
     }
 
     render() {
@@ -14,13 +20,18 @@ class ActivityInput extends Component {
             <div>
                 <h1>Add New Activity</h1>
                 <form>
-                    <span>Name: <input type="text" name="activityName" id="text" /></span>
-                    <span>Description: <input type="text" name="activityDescription" id="text" /></span>
-                    <span>Duration: <input type="text" name="activityDuration" id="text" /></span>
-                    <span>What to bring: <input type="text" name="activityWhatToBring" id="text" /></span>
-                    <span>What to do: <input type="text" name="activityWhatToDo" id="text" /></span>
+                    <span>Name: <input type="text" name="activityName" id="text" onChange={this.handleOnChange} /></span>
+                    <span>Description: <input type="text" name="activityDescription" id="text" onChange={this.handleOnChange} /></span>
+                    <span>Duration: <input type="text" name="activityDuration" id="text" onChange={this.handleOnChange} /></span>
+                    <span>What to bring: <input type="text" name="activityWhatToBring" id="text" onChange={this.handleOnChange} /></span>
+                    <span>What to do: <input type="text" name="activityWhatToDo" id="text" onChange={this.handleOnChange} /></span>
                     <input type="submit" value="Submit" />
                 </form>
+                {[this.state.activityName,
+                this.state.activityDescription,
+                this.state.activityDuration,
+                this.state.activityWhatToBring,
+                this.state.activityWhatToDo]}
             </div>
         )
     }
