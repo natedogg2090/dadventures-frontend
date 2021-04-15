da2d2d1f2e6d6ce90abf292523cd21cc55ae1726
Nate Dietz <nsdietz@gmail.com>
2021-04-15 21:48:12 +0100

    refactored activityReducer

--- src/reducers/manageActivity.js
+++ src/reducers/manageActivity.js
@@ -3,22 +3,14 @@ export default function manageActivities(state = {
 }, action) {
 	switch (action.type) {
 		case 'ADD_ACTIVITY':
-            console.log('activity is being added')
-            console.log(action.text) 
             // {activityName: "tom", activityDescription: "go out", activityDuration: "20", activityWhatToBring: "shirt, shoes", activityWhatToDo: "1. walk. 2. run"}
-            debugger
-			const activity = {
-				text: action.text
-                
-			}
-			
+			const activity = action.text
 			return { ...state, activities: [...state.activities, activity] }
 
 		// case 'DELETE_RESTAURANT':
 		// 	return { ...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) }
 
 		// case 'ADD_REVIEW':
-		// 	debugger
 		// 	const review = { 
 		// 		text: action.review.text, 
 		// 		restaurantId: action.review.restaurantId, 
