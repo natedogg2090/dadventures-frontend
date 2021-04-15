5145a7cfdfdf7d9cb23b707b1235cd62deff97b8
Nate Dietz <nsdietz@gmail.com>
2021-04-15 21:30:42 +0100

    dispatch addActivity to reducer

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import ActivityInput from '../components/activities/ActivityInput';
 import Activities from '../components/activities/Activities';
+import { connect } from 'react-redux';
 
 class ActivityContainer extends Component {
     render () {
         return (
             <div>
-                <ActivityInput />
+                <ActivityInput addActivity={this.props.addActivity} />
                 <Activities />
             </div>
         )
     }
 }
 
-export default ActivityContainer
+const mapDispatchToProps = dispatch => ({
+    addActivity: text => dispatch({ type: "ADD_ACTIVITY", text})
+})
+
+export default connect(null, mapDispatchToProps) (ActivityContainer);
