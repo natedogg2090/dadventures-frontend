0ef20213349dc4f02b45997520987f2d99a516e8
Nate Dietz <nsdietz@gmail.com>
2021-04-15 21:52:40 +0100

    clear form fields after submission

--- src/components/activities/ActivityInput.js
+++ src/components/activities/ActivityInput.js
@@ -32,11 +32,11 @@ class ActivityInput extends Component {
             <div>
                 <h1>Add New Activity</h1>
                 <form onSubmit={this.handleOnSubmit}>
-                    <span>Name: <input type="text" name="activityName" id="text" onChange={this.handleOnChange} /></span>
-                    <span>Description: <input type="text" name="activityDescription" id="text" onChange={this.handleOnChange} /></span>
-                    <span>Duration: <input type="text" name="activityDuration" id="text" onChange={this.handleOnChange} /></span>
-                    <span>What to bring: <input type="text" name="activityWhatToBring" id="text" onChange={this.handleOnChange} /></span>
-                    <span>What to do: <input type="text" name="activityWhatToDo" id="text" onChange={this.handleOnChange} /></span>
+                    <span>Name: <input type="text" name="activityName" id="text" onChange={this.handleOnChange}  value={this.state.activityName} /></span>
+                    <span>Description: <input type="text" name="activityDescription" id="text" onChange={this.handleOnChange} value={this.state.activityDescription} /></span>
+                    <span>Duration: <input type="text" name="activityDuration" id="text" onChange={this.handleOnChange} value={this.state.activityDuration} /></span>
+                    <span>What to bring: <input type="text" name="activityWhatToBring" id="text" onChange={this.handleOnChange} value={this.state.activityWhatToBring} /></span>
+                    <span>What to do: <input type="text" name="activityWhatToDo" id="text" onChange={this.handleOnChange} value={this.state.activityWhatToDo} /></span>
                     <input type="submit" value="Submit" />
                 </form>
                 {[this.state.activityName,
