f6402578b535ac125f5528e0f6f19e4d1fbd3da6
Nate Dietz <nsdietz@gmail.com>
2021-05-03 21:49:33 +0100

    added custom font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageActivity from './reducers/manageActivity';
