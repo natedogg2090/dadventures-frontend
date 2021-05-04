89ac83b6774e1c12003804b688bfe2ac44010825
Nate Dietz <nsdietz@gmail.com>
2021-05-04 04:30:39 +0100

    added thunk

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import manageActivity from './reducers/manageActivity';
 
-const store = createStore(manageActivity)
+const store = createStore(manageActivity, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
