a800156ecb44f14b37dc55141df26929a2cb45b4
Nate Dietz <nsdietz@gmail.com>
2021-05-17 20:28:15 +0100

    add activities from api

--- src/reducers/manageActivity.js
+++ src/reducers/manageActivity.js
@@ -1,11 +1,26 @@
 export default function manageActivities(state = {
-	activities: []
+	activities: [], loading: false
 }, action) {
 	switch (action.type) {
+		case 'LOADING_ACTIVITIES':
+			return {
+				...state,
+				activities: [...state.activities],
+				loading: true
+			}
 		case 'ADD_ACTIVITY':
-            // {activityName: "tom", activityDescription: "go out", activityDuration: "20", activityWhatToBring: "shirt, shoes", activityWhatToDo: "1. walk. 2. run"}
-			const activity = action.text
-			return { ...state, activities: [...state.activities, activity] }
+            // {name: "tom", description: "go out", duration: 20, what_to_bring: "shirt, shoes", what_to_do: "1. walk. 2. run"}
+			// attributes: {name: "Roasting Marshmallows by the Campfire", description: "Roasting marshmallows by the campfire is a time ho…radition that has been passed on for generations.", duration: 60, what_you_need: "Marshmallows, Chocolate Bars, Graham Crackers, Long skewers", what_to_do: "Assuming the campfire already exits, 1. Attach the…ot marshmallow and pull out the skewer. 5. Enjoy!"}
+			// id: "1"
+			// relationships: {users: {…}, memories: {…}}
+			// type: "activity"
+			
+			const activity = action.activities
+			return { 
+				...state,
+				activities: activity,
+				loading: false
+			}
 
 		// case 'DELETE_RESTAURANT':
 		// 	return { ...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) }
