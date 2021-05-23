355d1b1e30576cecd95608a6953a6d077f207023
Nate Dietz <nsdietz@gmail.com>
2021-05-23 22:09:22 +0100

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import manageActivity from './reducers/manageActivity';
 
-const store = createStore(manageActivity, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(manageActivity, composeEnhancers( applyMiddleware(thunk) ))
 
 ReactDOM.render(
   <Provider store={store}>
