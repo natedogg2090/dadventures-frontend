9addb6af3706016067d7e71419f3717255e8b4ee
Nate Dietz <nsdietz@gmail.com>
2021-06-03 23:19:07 +0100

    created container for dad jokes

---
+++ src/containers/JokesContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchDadJokes } from '../actions/fetchDadJokes';
+import Jokes from '../components/jokes/Jokes';
+
+class JokeContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchDadJokes()
+    }
+
+    render () {
+        return (
+            <div class="jokesContainer">
+                <Jokes jokes={this.props.jokes} />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return{
+        jokes: state.manageJokes.jokes,
+        loading: state.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        fetchDadJokes: () => dispatch(fetchDadJokes()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps ) (JokeContainer);
