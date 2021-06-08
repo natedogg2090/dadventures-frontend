df624a60726b4bc241c12604b3710a29db63aa9b
Nate Dietz <nsdietz@gmail.com>
2021-06-08 22:59:02 +0100

    post new memory

--- src/actions/addMemory.js
+++ src/actions/addMemory.js
@@ -1,16 +1,16 @@
-// const baseURL = 'http://localhost:3000/api/v1/activities'
+const baseURL = 'http://localhost:3000/api/v1/activities/'
 
 export const addMemory = (data) => {
     return (dispatch) => {
-        // fetch(`${baseURL}`, {
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Accept': 'application/json'
-        //     },
-        //     method: 'POST',
-        //     body: JSON.stringify(data)
-        // })
-        // .then(response => response.json())
-        // .then(activity => dispatch({type: 'ADD_MEMORY', payload: activity.data}))
+        fetch(`${baseURL}/${data.activity_id}/memories`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(memory => dispatch({ type: 'ADD_MEMORY', payload: data }))
     }
 }
