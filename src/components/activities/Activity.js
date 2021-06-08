8c24df4253767d5d929c1a004b946ec74a14b7c0
Nate Dietz <nsdietz@gmail.com>
2021-06-08 23:00:33 +0100

    refactored to functional component

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import MemoriesContainer from '../../containers/MemoriesContainer';
 
-class Activity extends Component {
+// class Activity extends Component {
 
-    render() {
-        const { activity } = this.props;
+//     render() {
+//         console.log(this.props);
+//         const { activity } = this.props;
 
-        return(
-            <div class="activity" id={this.props.activityId}>
-                <h1>{activity.name}</h1>
-                <p>{activity.duration}</p>
-                <p>{activity.description}</p>
-                <p>{activity.what_to_bring}</p>
-                <p>{activity.what_to_do}</p>
-                < MemoriesContainer activity={activity} activityId={this.props.activityId} />                
-            </div>
-        )
-    }
+//         console.log(activity);
+
+//         return(
+//             <div class="activity">
+//                 <h1>{activity.name}</h1>
+//                 <p>{activity.duration}</p>
+//                 <p>{activity.description}</p>
+//                 <p>{activity.what_to_bring}</p>
+//                 <p>{activity.what_to_do}</p>
+//                 < MemoriesContainer activity={activity} activityId={this.props.activityId} />                
+//             </div>
+//         )
+//     }
+// }
+
+const Activity = (props) => {
+    let activity = props.activities[props.match.params.id - 1]
+    console.log(props)
+
+    return (
+        <div>
+            <h1>{ activity ? activity.attributes.name : null }</h1>
+            <p>{ activity ? activity.attributes.duration : null}</p>
+            <p>{activity ? activity.attributes.description : null}</p>
+            <p>{activity ? activity.attributes.what_to_bring : null}</p>
+            <p>{activity ? activity.attributes.what_to_do : null}</p>
+            < MemoriesContainer activity={activity} />
+        </div>
+    )
 }
 
 export default Activity;
