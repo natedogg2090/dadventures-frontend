3fff7178ebf35f9b6a01fa27255c6c446aa8944e
Nate Dietz <nsdietz@gmail.com>
2021-06-09 23:35:30 +0100

    refactored removing excess code

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,31 +1,8 @@
-// import React, { Component } from 'react';
 import React from 'react';
 import MemoriesContainer from '../../containers/MemoriesContainer';
 
-// class Activity extends Component {
-
-//     render() {
-//         console.log(this.props);
-//         const { activity } = this.props;
-
-//         console.log(activity);
-
-//         return(
-//             <div class="activity">
-//                 <h1>{activity.name}</h1>
-//                 <p>{activity.duration}</p>
-//                 <p>{activity.description}</p>
-//                 <p>{activity.what_to_bring}</p>
-//                 <p>{activity.what_to_do}</p>
-//                 < MemoriesContainer activity={activity} activityId={this.props.activityId} />                
-//             </div>
-//         )
-//     }
-// }
-
 const Activity = (props) => {
     let activity = props.activities[props.match.params.id - 1]
-    console.log(props)
 
     return (
         <div>
