65700defa7efd6e1e7df0b435ada489cb31e97c1
Nate Dietz <nsdietz@gmail.com>
2021-06-09 22:40:02 +0100

    Memory reducer

--- src/reducers/manageMemories.js
+++ src/reducers/manageMemories.js
@@ -1,38 +1,18 @@
-export default function manageMemories(state = [], action) {
+export default function manageMemories(state = {memories: [], loading: false}, action) {
 	switch (action.type) {
-		// case 'LOADING_ACTIVITIES':
-		// 	return {
-		// 		...state,
-		// 		activities: [...state.activities],
-		// 		loading: true
-		// 	}
-		// case 'ADD_ACTIVITY':
-		// 	if (!(Array.isArray(action.payload))) {
-		// 		return {
-		// 			...state,
-		// 			activities: [...state.activities, action.payload],
-		// 			loading: false
-		// 		}
-		// 	}
-		// 	else {
-		// 		return { 
-		// 			...state,
-		// 			activities: action.payload,
-		// 			loading: false
-		// 		}
-		// 	}
-				
-		// case 'DELETE_RESTAURANT':
-		// 	return { ...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) }
-
-		// case 'ADD_REVIEW':
-		// 	const review = { 
-		// 		text: action.review.text, 
-		// 		restaurantId: action.review.restaurantId, 
-		// 		id: cuidFn() 
-		// 	}
-			
-		// 	return { ...state, reviews: [...state.reviews, review] }
+		case 'LOADING_MEMORIES':
+		return {
+			...state,
+			memories: [...state.memories],
+			loading: true
+		}
+		case 'ADD_MEMORIES':
+			const memories = action.payload
+			return { 
+				...state,
+				memories: [...state.memories, memories],
+				loading: false
+			}
 
 		// case 'DELETE_REVIEW':
 		// 	return { ...state, reviews: state.reviews.filter(review => review.id !== action.id) }
