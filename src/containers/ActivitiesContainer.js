7aa7ffbd758686dac70aff53a56e1e6ade4be5f8
Nate Dietz <nsdietz@gmail.com>
2021-06-13 22:25:29 +0100

    added Switch

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ActivityInput from '../components/activities/ActivityInput';
 import Activities from '../components/activities/Activities';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { fetchActivities } from '../actions/fetchActivities';
 import { addActivity } from '../actions/addActivity';
 import Activity from '../components/activities/Activity';
@@ -16,9 +16,11 @@ class ActivitiesContainer extends Component {
     render () {
         return (
             <div class="activitiesContainer">
-                <Route path='/activities/new' render={ () => <ActivityInput addActivity={this.props.addActivity} /> } />
-                <Route path='/activities/:id' render={ (routerProps) => <Activity { ...routerProps } activities={this.props.activities} /> } />
-                <Route exact path='/activities' render={ (routerProps) => <Activities { ...routerProps } activities={this.props.activities} /> } />
+                <Switch>
+                    <Route path='/activities/new' render={ () => <ActivityInput addActivity={this.props.addActivity} /> } />
+                    <Route path='/activities/:id' render={ (routerProps) => <Activity { ...routerProps } activities={this.props.activities} /> } />
+                    <Route exact path='/activities' render={ (routerProps) => <Activities { ...routerProps } activities={this.props.activities} /> } />
+                </Switch>
             </div>
         )
     }
