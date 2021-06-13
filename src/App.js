ba2c52f96e571174706ffef399e942af8f8ea962
Nate Dietz <nsdietz@gmail.com>
2021-06-13 22:46:26 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ActivityContainer from './containers/ActivitiesContainer';
 import JokesContainer from './containers/JokesContainer';
-// import MemoriesContainer from './containers/MemoriesContainer';
 
 class App extends Component {
   render() {
@@ -9,7 +8,6 @@ class App extends Component {
       <div>
         <ActivityContainer />
         <JokesContainer />
-        {/* <MemoriesContainer /> */}
       </div>
     );
   }
