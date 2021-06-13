62aca1c6650acb9138ecd995773b577973005416
Nate Dietz <nsdietz@gmail.com>
2021-06-13 22:16:35 +0100

    rendering all memories for a given activity

--- src/components/memories/Memory.js
+++ src/components/memories/Memory.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const Memory = (props) => {
-    // let activity = props.activities[props.match.params.id - 1]
-    // let memory = props.memories
 
     return (
-        // <p>{memory ? memory.attributes.description : null}</p>
-        <div>{props.memory.description}</div>
+        <li>{props.memory.description}</li>
     )
 }
 
