5c42ce4643f76abd97d3d72e664b297e41f50a56
Nate Dietz <nsdietz@gmail.com>
2021-06-15 22:59:54 +0100

    added basic nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import ActivityContainer from './containers/ActivitiesContainer';
 import JokesContainer from './containers/JokesContainer';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <nav>
+            <Link to="/activities/new">New Activity</Link> | 
+            <Link to="/activities/">Activities</Link>
+        </nav>
         <ActivityContainer />
         <JokesContainer />
       </div>
